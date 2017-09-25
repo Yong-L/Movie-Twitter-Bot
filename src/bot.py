1e2ae68753d377a46fdae4f29c65588ec53e3da3
Yong-L <dkrcjs7521@gmail.com>
2017-09-25 16:59:08 -0400

    Using tweepy api streamer

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,7 @@
 import tweepy
 from secrets import *
+import requests
+from bs4 import BeautifulSoup
 
 # Create OAuthHandler
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -11,5 +13,19 @@ api = tweepy.API(auth)
 
 public_tweets = api.home_timeline()
 
+#Prints the tweets that are public
 for tweet in public_tweets:
     print(tweet.text)
+
+class TweetStreamer(tweepy.StreamListener):
+    
+    #New status 
+    def on_status(self, status):
+        username = status.user.screen_name
+        status_id = status.id
+
+        print("Works")
+
+movie_bot = TweetStreamer()
+stream = tweepy.Stream(auth, movie_bot)
+stream.filter(track=['bot_username'])
