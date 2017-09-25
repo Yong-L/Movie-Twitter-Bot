f374470409187946ccb29a9bddc5c053fd74451d
Yong-L <dkrcjs7521@gmail.com>
2017-09-25 17:27:09 -0400

    Working streamer

--- src/bot.py
+++ src/bot.py
@@ -3,29 +3,22 @@ from secrets import *
 import requests
 from bs4 import BeautifulSoup
 
-# Create OAuthHandler
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
 auth.set_access_token(access_key, access_secret)
-
-#Construct API instance
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-
-#Prints the tweets that are public
-for tweet in public_tweets:
-    print(tweet.text)
-
+#Override streamer
 class TweetStreamer(tweepy.StreamListener):
     
     #New status 
     def on_status(self, status):
-        username = status.user.screen_name
-        status_id = status.id
+        print(status.text)
 
-        print("Works")
+#movie_bot = TweetStreamer()
+#stream = tweepy.Stream(auth, movie_bot)
+#stream.filter(track=['bot_username'])
 
-movie_bot = TweetStreamer()
-stream = tweepy.Stream(auth, movie_bot)
-stream.filter(track=['bot_username'])
+if __name__ == '__main__':
+    stream_listener = TweetStreamer()
+    twitter_stream = tweepy.Stream(auth, stream_listener)
+    twitter_stream.filter(track=['yongsulee000'])
