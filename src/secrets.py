e4c2ef4a6a3eda6e94189f41946ec089081d1a4d
Yong-L <dkrcjs7521@gmail.com>
2017-09-26 14:28:25 -0400

    Credentials python file

---
+++ src/secrets.py
@@ -0,0 +1,8 @@
+#secret.py
+#Credentials for twitter access
+
+consumer_key = 'xxxxxxxxxxxxxxxxx'
+consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+access_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+access_secret = 'xxxxxxxxxxxxxxxxxxxxxxxx'
+bot_username = "xxxxxxxxxxxxx"
